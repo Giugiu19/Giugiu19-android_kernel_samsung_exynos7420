dd1b87eff0e3b26da754487b45fdc4380e694431
GDevGroup <gpirasking@outlook.it>
2017-11-18 19:23:28 +0100

    Switch to Linaro

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=/home/giugiu19/aarch64-linux-eabi-UB-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/giugiu19/aarch64-linaro-linux-android/bin/aarch64-linaro-linux-android-
 
 make ARCH=arm64 zerofltexx_defconfig
 make ARCH=arm64 -j4
